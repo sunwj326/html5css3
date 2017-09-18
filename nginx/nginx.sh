b942b249b2c11ea786a6fc8d369e07ecd787573a
James Sun <james@JamesdeMac-mini.local>
2017-09-18 20:49:05 +0800

    function.js and node.sh

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,1 +1,1 @@
-docker run --name webserver -d -p 8010:80 -v $(pwd):/var/www/html -v $(pwd)/default.conf:/etc/nginx/conf.d/default.conf:ro nginx
+docker run --name webserver -d -p 8035:80 -v $(pwd)/../:/var/www/html -v $(pwd)/default.conf:/etc/nginx/conf.d/default.conf:ro daocloud.io/library/nginx
---
+++ nodeapp/node.sh
@@ -0,0 +1,1 @@
+docker run -it --rm -it --name running-script -v $(pwd)/..:/code -w /code daocloud.io/library/node:6.11.2-alpine node $1 
